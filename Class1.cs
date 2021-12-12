402c2587c1b50fefa44b959836cec6fe8207e93d
KuzX_yb28@itstepru.onmicrosoft.com <KuzX_yb28@itstepru.onmicrosoft.com>
2021-12-12 18:56:58 +0300

    HW STRUCTS + CLASSES

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSandBox
+{
+    partial class carr
+    {
+        partial void partial_method()
+        {
+            Console.WriteLine("Частичный метод");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,232 +6,189 @@ using System.Text;
 using System.Threading.Tasks;
 namespace CSharpSandBox
 {
+	struct car
+	{
+		private double clearance;
+		private double eng_vol;//объём двигателя
+		private double eng_power;//мощность двигателя
+		private double wheel_d;
+		private int color_id;
+		private string[] color;
+		private string[] shifter;
+		private int shifter_id;
+		public void print()
+		{
+			if (eng_vol == 0) eng_vol = 3.0;//типа конструктор по умолчанию, простите за костыль, т.к. в C# нельзя переопределить конструктор по умолчанию, я решил сделать так
+			if (clearance == 0) clearance = 130;
+			if (eng_power == 0) eng_power = 225;
+			if (wheel_d == 0) wheel_d = 15;
+			if (color_id == 0)
+			{
+				color = new string[3] { "Красный", "Зелёный", "Жёлтый" };
+				Random r = new Random();
+				color_id = r.Next(0, 3);
+			}
+			if (shifter_id == 0)
+			{
+				Random r = new Random();
+				shifter_id = r.Next(0, 2);
+				shifter = new string[2] { "Механическая", "Автоматическая" };
+			}
+			Console.WriteLine(
+				$"Высота подвески(мм): {clearance}\n" +
+				$"Объём двигателя(л): {eng_vol}\n" +
+				$"Мощность двигателя(л.с.) {eng_power}\n" +
+				$"Диаметр колеса: { wheel_d} \n" +
+				$"Цвет: {color[color_id]}\n" +
+				$"Тип коробки передач: {shifter[shifter_id]}");
+		}
+		public double getset_clearence
+		{
+			get { return clearance; }
+			set { clearance = value; }
+		}
+		public double getset_eng_vol
+		{
+			get { return eng_vol; }
+			set { eng_vol = value; }
+		}
+		public double getset_eng_power
+		{
+			get { return eng_power; }
+			set { eng_power = value; }
+		}
+		public string getset_color
+		{
+			get { return color[color_id]; }
+			set { color[color_id] = value; }
+		}
+		public double getset_wheel_d
+		{
+			get { return wheel_d; }
+			set { wheel_d = value; }
+		}
+		public string getset_shifter
+		{
+			get { return shifter[shifter_id]; }
+			set { shifter[shifter_id] = value; }
+		}
+	}
+	partial class carr
+	{
+		private string name;
+		private double clearance;
+		private double eng_vol;//объём двигателя
+		private double eng_power;//мощность двигателя
+		private static double wheel_d;
+		private string color;
+		private static string shifter;
+		public carr(string name, double clearence, double eng_vol, double eng_power, string color)
+		{
+			this.name = name;
+			this.clearance = clearence; 
+			this.eng_power = eng_power;
+			this.eng_vol = eng_vol;
+			this.color = color;
+		}
+        public carr()
+        {
+			name = "Машина без названия";
+			clearance = 130;
+			eng_power = 225;
+			eng_vol = 3.0;
+			color = "Белый";
+			shifter = "Автоматическая";
+        }
+		partial void partial_method();
+		public void obvertka() { partial_method(); } //для того чтобы вызвать частичный метод, т.к. его нельзя сделать публичным
+		static carr()
+        {
+			shifter = "Механическая";
+			wheel_d = 15;
+        }
+		public void print()
+		{
+			Console.WriteLine(
+				$"\n--------------------------------------\n" +
+				$"Название автомобиля: {name}\n" +
+				$"Высота подвески(мм): {clearance}\n" +
+				$"Объём двигателя(л): {eng_vol}\n" +
+				$"Мощность двигателя(л.с.) {eng_power}\n" +
+				$"Диаметр колеса: { wheel_d} \n" +
+				$"Цвет: {color}\n" +
+				$"Тип коробки передач: {shifter}");
+		}
+		public double getset_clearence
+		{
+			get { return clearance; }
+			set { clearance = value; }
+		}
+		public double getset_eng_vol
+		{
+			get { return eng_vol; }
+			set { eng_vol = value; }
+		}
+		public double getset_eng_power
+		{
+			get { return eng_power; }
+			set { eng_power = value; }
+		}
+		public string getset_color
+		{
+			get { return color; }
+			set { color = value; }
+		}
+		public double getset_wheel_d
+		{
+			get { return wheel_d; }
+			set { wheel_d = value; }
+		}
+		public string getset_shifter
+		{
+			get { return shifter; }
+			set { shifter = value; }
+		}
+		public string getset_name {
+			get { return name; }
+			set { name = value; }
+		}
+	}
 	class Program
 	{
-		static void stringdz1()
-		{
-			//Предоставление пользователю возможности выбрать строки для объединения
-			Console.WriteLine("Первая строка: ПЕРВОЕ СЛОВО второе, третье ЧЕТВЁРТОЕ пятое");
-			Console.WriteLine("Вторая строка: 1, 2, три, четыре, ПяТь");
-			Console.WriteLine("Третья строка: В эТоЙ СтРоКе ПяТь СлОв");
-			Console.WriteLine("Какие две строки объединить? (1/2)");
-			int input = Convert.ToInt32(Console.ReadLine());//ввод пользователя
-			string str1 = null;//объявление первой строки
-			if (input == 1)
-				str1 = "ПЕРВОЕ СЛОВО второе, третье ЧЕТВЁРТОЕ пятое";//инициализация первой строки
-			if (input == 2)
-				str1 = "1, 2, три, четыре, ПяТь";//инициализация первой строки
-			if (input == 3)
-				str1 = "В эТоЙ СтРоКе ПяТь СлОв";//инициализация первой строки
-			Console.WriteLine("Какие строки объединить? (2/2)");//объявление второй строки
-			input = Convert.ToInt32(Console.ReadLine());
-			string str2 = null;
-			if (input == 1)
-				str2 = "ПЕРВОЕ СЛОВО второе, третье ЧЕТВЁРТОЕ пятое";//инициализация второй строки
-			if (input == 2)
-				str2 = "1, 2, три, четыре, ПяТь";//инициализация второй строки
-			if (input == 3)
-				str2 = "В эТоЙ СтРоКе ПяТь СлОв";//инициализация второй строки
-			string str3;//объявление "объеденяющей" строки
-			if (str1.First().CompareTo(str2.First()) < 0)//если первая буква "меньше" (т.е "первее" по алфавиту),
-				str3 = str1 + str2;						//тогда данная строка будет отображена на первом месте
-			else str3 = str2 + str1;//в ином случае, другая строка будет на первом месте
-			Console.WriteLine(str3);//вывод исходной строки
-		}
-		static void stringdz2()
+		static void structs()
         {
-			string str = "Какая-то строка";
-			Console.WriteLine(str);
-			string substr1 = str.Substring(0, 1);
-			string substr2 = str.Substring(0, 2);
-			string substr3 = str.Substring(0, 3);
-			Console.WriteLine(
-				$"1 подстрока: {substr1}\n" +
-				$"2 подстрока: {substr2}\n" +
-				$"3 подстрока: {substr3}\n" +
-				$"Сортировка по убыванию: ");
-			if (substr1.Length > substr2.Length && substr1.Length > substr3.Length)
-			{
-				Console.Write(substr1 + " -- ");
-				if (substr2.Length > substr3.Length)
-					Console.Write(substr2 + " -- " + substr3);
-				else
-					Console.Write(substr3 + " -- " + substr2);
-
-			}
-
-			else if (substr2.Length > substr1.Length && substr2.Length > substr3.Length)
-			{
-				Console.Write(substr2 + " -- ");
-				if(substr1.Length>substr3.Length)
-					Console.Write(substr1 + " -- " + substr3);
-				else 
-				Console.Write(substr3 + " -- " + substr1);
-			}
-			else if (substr3.Length > substr1.Length && substr3.Length > substr2.Length)
-            {
-				Console.Write(substr3 + " -- ");
-				if (substr1.Length > substr2.Length)
-					Console.Write(substr1 + " -- " + substr2);
-				else
-					Console.Write(substr2 + " -- " + substr1);
-			}
-			Console.WriteLine();
-
-		}
-		static void stringdz3()
-        {
-			string str = "eveeen odd eveeen even even even evenee eveeeeen even odddd ev1n ev odd o o o e2en ev"; //odd - 3 элемента(нечётный), even - 4 элемента(чётный)
-			string[] strr = str.Split(' '); //методом split отделяем слова по пробелу
-			int counter = 0;//счётчик пригодится позже 
-			for (int i = 0; i < strr.Length; i++)//считаем чётные строки для выделения памяти
-            {
-				if (strr[i].Length % 2 == 0)
-					counter++; //even
-            }
-			string[] odd = new string[strr.Length - counter]; //объявляем массив нечётных строк (в counter мы занесли чётные строки, поэтому длина массива = length - counter, т.е. вычитаем из массива чётные строки, остаются нечётные)
-			string[] even = new string[counter];//объявляем массив чётных строк
-			int even_counter = 0; int odd_counter = 0;//итератор для каждого из массивов
-			string[] buffer = new string[strr.Length];//буффер пригодится для того чтобы заносить в него одинаковые элементы
-			for (int i = 0; i < strr.Length; i++)//выделение памяти под чётные строки
-			{
-				if (strr[i].Length % 2 == 0) //если кол-во символов в строке делистя на 2, значит строка чётная
-				{
-					even[even_counter] = strr[i];//добавляем элемент в массив
-					even_counter++;//инкремент итератора для чётных строк
-				}
-				else 
-				{
-					odd[odd_counter] = strr[i];//то же самое, но с нечётными
-					odd_counter++;
-				}
-			}
-		
-			Console.Write("Even: ");
-			counter = 0;//обнуление счётчика, т.к. он нам пригодится для другого
-			for (int i = 0; i < even.Length; i++)//вывод массива на экран + нахождение одинаковых слов
-			{
-				Console.Write(even[i] + " ");
-				for (int j = 0; j < even.Length; j++)
-				{
-					if (even[i] == even[j])//если найдено совпадение
-					{
-						if (i != j)//если это не один и тот же элемент
-						{
-							buffer[j] = even[j];//сделал присваивание в буффер, чтобы избежать повторного счётчика одних и тех же строк. если сделать через счётчик, то в данном примере выдаст 18, хотя ответ - 8
-						}
-					}
-				}
-			}
-			for (int i = 0; i < buffer.Length; i++)
-			{
-				if (buffer[i] != null) counter++;
-			}
-			Console.WriteLine($"\nОбщих элементов: {counter}");
-            for (int i = 0; i < buffer.Length; i++)//зануление буффера
-            {
-				buffer[i] = null;
-            }
-			/////////////////////////////////////////Абсолютно то же самое, но с нечётными///////////////////////
-			Console.Write("Odd: ");
-			counter = 0;
-			for (int i = 0; i < odd.Length; i++)
-			{
-				Console.Write(odd[i] + " ");
-				for (int j = 0; j < odd.Length; j++)
-				{
-					if (odd[i] == odd[j])
-					{
-						if (i != j)
-						{
-							buffer[j] = odd[j];
-						}
-					}
-				}
-			}
-			for (int i = 0; i < buffer.Length; i++)
-            {
-				if (buffer[i] != null) counter++; 
-            }
-			Console.WriteLine($"\nОбщих элементов: {counter}");
-		}
-
-		static void vedom()
-        {
-			Console.WriteLine("ФИО");
-			string fio = Console.ReadLine();
-			Console.WriteLine("------------------------------------------------");
-			Console.WriteLine("Введите количество предметов");
-			int count = Convert.ToInt32(Console.ReadLine());
-			Console.WriteLine($"Количество предметов в ведомстве: {count}");
-			int[][] vedom = new int[count][];
-			string[] subjs = new string[count];
-			//Заполняем массив subjs предметами
-			Console.WriteLine("------------------------------------------------");
-			Console.Write("Введите предметы\n");
-            for (int i = 0; i < subjs.Length; i++)
-            {
-				Console.Write($"({i+1}/{count}) ");
-				subjs[i] = Console.ReadLine();
-            }
-			//узнаём кол-во оценок по каждому из предметов
-			Console.WriteLine("------------------------------------------------");
-			Console.Write($"Введите кол-во оценок по предмету:\n");
-			for (int i = 0; i < subjs.Length; i++)
-            {
-				Console.Write($"{subjs[i]}\n");
-				count = Convert.ToInt32(Console.ReadLine());
-				Console.WriteLine($"Количество оценок: {count}");
-				vedom[i] = new int[count];//создаём массив с оценками
-            }
-			Console.WriteLine("------------------------------------------------");
-			
-			Console.Write($"Введите оценки по предмету \n");
-			for (int i = 0; i < subjs.Length; i++)
-            {
-				Console.Write($"{subjs[i]}\n");
-
-				for (int j = 0; j < vedom[i].Length; j++)
-                {
-				fail:
-					Console.Write($"({j+1} / {vedom[i].Length})\n");
-					vedom[i][j] = Convert.ToInt32(Console.ReadLine());
-					if (vedom[i][j] > 5)
-					{
-						Console.WriteLine("Пожалуйста, укажите оценки по пятибальной шкале");
-						goto fail;
-					}
-                }
-				Console.WriteLine();
-            }
-			//вывод на экран
-			Console.Write($"ФИО Студента: {fio}\n");
-            for (int i = 0; i < subjs.Length; i++)
-            {
-				Console.Write($"Оценки по предмету {subjs[i]}: ");
-                for (int j = 0; j < vedom[i].Length; j++)
-                {
-					Console.Write($"{vedom[i][j]}, ");
-                }
-				Console.WriteLine();
-            }
-			Console.Write(fio);
-            for (int i = 0; i < subjs.Length; i++)
-            {
-				if (vedom[i].Average() > 2.5 && vedom[i].Length >= 2)
-					 Console.WriteLine($" аттестован по предмету {subjs[i]}\n");
-				else Console.WriteLine($" не аттестован по предмету {subjs[i]}");
-			}
-
+			car SUPRA = new car();
+			Console.WriteLine("Супра до тюнинга: ");
+			SUPRA.print();
+			SUPRA.getset_clearence = 20;
+			SUPRA.getset_color = "Фиолетовый";
+			SUPRA.getset_shifter = "Автомат";
+			SUPRA.getset_eng_vol = 4;
+			SUPRA.getset_eng_power = 300;
+			Console.WriteLine("\nСупра после тюнинга: ");
+			SUPRA.print();
         }
-		
 		static void Main(string[] args)
 		{
-			//stringdz1(); 
-			//stringdz2();
-			//stringdz3();
-			vedom();
+			//structs(); //Структура
+			//Класс:
+			carr supra = new carr();
+			supra.getset_name = "Toyota Supra";
+			supra.print();
+			supra.getset_clearence = 50; supra.getset_shifter = "Автоматическая";
+			supra.print();
+			carr VAZ_2107 = new carr("ВАЗ 2107",50,1.5,8,"Чёрный");
+			VAZ_2107.print();
+			VAZ_2107.getset_shifter = "Механическая";
+			VAZ_2107.print();
+			carr[] car_arr = new carr[5];
+			Console.WriteLine("Машины из массива: ");
+			for (int i = 0; i < car_arr.Length; i++)
+			{
+				car_arr[i] = new carr();
+				car_arr[i].print();
+			}
+			supra.obvertka();
 		}
-
 	}
 }
